a38d727c781ebda3e34edc5c9c288c66eb65e59a
st-prasad <trindo.me@gmail.com>
2023-11-26 19:54:52 +0530

    added dummy products data

---
+++ frontend/src/products.js
@@ -0,0 +1,83 @@
+const products = [
+    {
+      _id: '1',
+      name: 'Cinnamon Stick Bundle',
+      image: '/images/cinnamon_sticks.jpg',
+      description:
+        'A bundle of high-quality cinnamon sticks, perfect for adding rich flavor to your teas, desserts, and recipes. Handpicked and naturally sun-dried for optimal taste.',
+      brand: 'Spice Haven',
+      category: 'Cinnamon',
+      price: 12.99,
+      countInStock: 15,
+      rating: 4.8,
+      numReviews: 10,
+    },
+    {
+      _id: '2',
+      name: 'Cinnamon Chai Tea Blend',
+      image: '/images/cinnamon_chai_tea.jpg',
+      description:
+        'Indulge in the warm and aromatic flavors of our Cinnamon Chai Tea Blend. Carefully crafted with a blend of premium black tea leaves and aromatic cinnamon spices.',
+      brand: 'Tea Delights',
+      category: 'Tea',
+      price: 9.99,
+      countInStock: 20,
+      rating: 4.5,
+      numReviews: 8,
+    },
+    {
+      _id: '3',
+      name: 'Cinnamon Infused Honey',
+      image: '/images/cinnamon_honey.jpg',
+      description:
+        'Experience the sweet harmony of natural honey infused with the warm essence of cinnamon. Perfect for drizzling on toast, pancakes, or adding a touch of sweetness to your tea.',
+      brand: 'Golden Harvest',
+      category: 'Honey',
+      price: 14.99,
+      countInStock: 12,
+      rating: 4.2,
+      numReviews: 15,
+    },
+    {
+      _id: '4',
+      name: 'Cinnamon Essential Oil',
+      image: '/images/cinnamon_oil.jpg',
+      description:
+        'Pure and potent cinnamon essential oil for aromatherapy and various wellness applications. Extracted from premium cinnamon bark, known for its rich and comforting scent.',
+      brand: 'Natures Essence',
+      category: 'Essential Oil',
+      price: 24.99,
+      countInStock: 8,
+      rating: 4.9,
+      numReviews: 18,
+    },
+    {
+      _id: '5',
+      name: 'Cinnamon Spice Cake Mix',
+      image: '/images/cinnamon_cake_mix.jpg',
+      description:
+        'Create mouthwatering cinnamon spice cakes with our premium cake mix. Just add a few simple ingredients, and enjoy the delightful aroma and taste of homemade cinnamon goodness.',
+      brand: 'Bake Bliss',
+      category: 'Baking',
+      price: 16.99,
+      countInStock: 10,
+      rating: 4.7,
+      numReviews: 14,
+    },
+    {
+      _id: '6',
+      name: 'Cinnamon Scented Candle Set',
+      image: '/images/cinnamon_candles.jpg',
+      description:
+        'Set the mood with our cinnamon scented candle set. Each candle is hand-poured with natural wax, releasing a warm and inviting cinnamon fragrance for a cozy atmosphere.',
+      brand: 'Aromatherapy Delights',
+      category: 'Home Decor',
+      price: 19.99,
+      countInStock: 18,
+      rating: 4.6,
+      numReviews: 12,
+    },
+  ];
+  
+  export default products;
+  
