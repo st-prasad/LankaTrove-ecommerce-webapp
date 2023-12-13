aee65c8bd5835a200f9c7ce3f08e8583b7a8dcdf
st-prasad <trindo.me@gmail.com>
2023-12-13 23:59:15 +0530

    To overcome the browser Same-Origin Policy during development

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    // proxy requests prefixed '/api' and '/uploads'
+    proxy: {
+      '/api': 'http://localhost:5000',
+      '/uploads': 'http://localhost:5000',
+    },
+  },
+});
