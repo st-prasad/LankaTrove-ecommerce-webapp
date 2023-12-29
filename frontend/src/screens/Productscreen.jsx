5826cc01173c7ff8e2281ea7f5b97fa6de1707c2
st-prasad <trindo.me@gmail.com>
2023-12-29 18:50:21 +0530

    productscreen content without ui

--- frontend/src/screens/Productscreen.jsx
+++ frontend/src/screens/Productscreen.jsx
@@ -1,29 +1,32 @@
 import { useParams } from "react-router-dom";
 //The useParams hook helps us to access the URL parameters from a current route. It returns an object of key/value pairs of URL parameters. Use it to access match.params of the current <Route>.
+import { Button, Card, Col, Container, Image, ListGroup, Row } from "react-bootstrap";
 import productsData from "../productsData";
 
-import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import Rating from "../components/Rating";
 
 const Productscreen = () => {
   const { id: productId } = useParams();
   const productData = productsData.find((element) => element._id === productId);
 
-  return <Card>
+  return <Container>
        
-      {/* <Card.Img
-        variant="top"
+       <Link className="btn product-backbtn" to={"/"}>
+        Back </Link>
+        <Image
         src={productData.image}
         alt={productData.name}
         className="product-image"
-      /> */}
-      
-      <Card.Body>
-        <Card.Title as='div' className="product-name"><h1>{productData.name}</h1></Card.Title>
+        fluid
+      />
+        <div className="product-name">{productData.name}</div>
 
-        <Card.Text as='div' className="product-description">{productData.description}</Card.Text>
-        <Card.Text as='h3' className="product-price">${productData.price}</Card.Text>
-      </Card.Body>
-    </Card>
+        <div className="product-description">{productData.description}</div>
+        <h3 className="product-price">${productData.price}</h3>
+        <div>
+          <Rating value={productData.rating} reviewsCount={productData.numReviews} />
+        </div>
+    </Container>
 };
 export default Productscreen;
